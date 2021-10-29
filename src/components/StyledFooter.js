a65575f0107368561b4c6f398760c9683d1a36c0
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-10-29 17:45:25 -0500

    Styled Footer with cta button

--- src/components/StyledFooter.js
+++ src/components/StyledFooter.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "gatsby"
 
 const StyledFooter = () => {
     return (
@@ -12,10 +13,22 @@ const StyledFooter = () => {
           style={{
             backgroundColor: "#121212",
             color: "#F7F4F0",
-            padding: "48px",
+            padding: "0 0 48px 0",
             margin: "0 auto",
           }}
         >
+          <Link to="/contact"><p
+            style={{
+              textAlign: "center",
+              margin: "0",
+              padding: "0 0 120px 0",
+              fontFamily: "var(--main-font)",
+              fontSize: "248px",
+              lineHeight: "0.7"
+            }}
+          >
+            start a project →
+          </p></Link>
           <p
             style={{
               textAlign: "center",
