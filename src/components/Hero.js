90ce708c17d99211972ae90f3fe653fa5304fa93
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-11-15 16:40:57 -0600

    Remove cta button from hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -30,13 +30,6 @@ const Hero = () => {
           />
         </div>
 
-        <div className="container cssanimation sequence fadeInBottom">
-          <div id="home-page-cta">
-            <Link to="/work">
-              <button>Learn more</button>
-            </Link>
-          </div>
-        </div>
       </div>
     )
 }
