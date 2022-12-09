4f8cfababe1412e5f720b32462bf32e440cc5ebb
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-12-09 16:03:08 -0600

    Update contact page with layout and email

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,25 +7,22 @@ import "../components/main.css"
 import "../components/layout.css"
 
 import Seo from "../components/seo"
+import Layout from "../components/layout"
 
 
 
 const contact = () => {
   return (
-    <div className="container contact-page cssanimation sequence fadeInBottom">
-      <Seo title="Contact" />
-      <h1 style={{ fontWeight: "bold", fontSize: "32px" }}>conjure</h1>
-      <h2 id="inquiries">CONNECT</h2>
-
-      <div className="icons contact-link">
-        <a href="https://www.linkedin.com/in/ryan-smith-219614230/">
-          <SiLinkedin size="32px" color="#0072b1" />
-        </a>
+    <Layout>
+      <div className="container contact-page cssanimation sequence fadeInBottom">
+        <Seo title="Contact" />
+        <h2 id="inquiries">INQUIRIES:</h2>
+        <h2 id="email">RYAN@CONJURE.COMPANY</h2>
+        <Link to="/">
+          <button>Home</button>
+        </Link>
       </div>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
-    </div>
+    </Layout>
   )
 }
 
